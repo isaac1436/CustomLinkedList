ea45339758924da648d387d513d8f39d91aefc71
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-05-10 11:34:22 +0100

    Add project files.

---
+++ CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,132 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures
+{
+    public class CustomLinkedList<T>
+    {
+        public ListNode<T> Head { get; set; }
+        public ListNode<T> Tail { get; set; }
+
+        public ListNode<T> CurrNode { get; set; }
+
+        public void AddItem(T item)
+        {
+            if (Head is null)
+            {
+                Head = Tail = new ListNode<T>(item);
+            }
+            else
+            {
+                var temp = Tail;
+                var newNode = new ListNode<T>(item, temp);
+                Tail.NextNode = newNode;
+                Tail = newNode;
+            }
+        }
+
+        public void AddNode(ListNode<T> node)
+        {
+            if (Head is null)
+            {
+                Head = Tail = node;
+            }
+            else
+            {
+                Tail.NextNode = node;
+                Tail = node;
+            }
+
+        }
+
+        public T AddFirst(int count)
+        {
+            dynamic sum = 0;
+            var temp = Head;
+            for (int i = 0; i < count; i++)
+            {
+                sum += Head.Value;
+                Head = Head.NextNode;
+
+            }
+            Head = temp;
+            return sum;
+        }
+
+        public T AddLast(int count)
+        {
+            dynamic sum = 0;
+            var temp = Tail;
+            for (int i = 0; i < count; i++)
+            {
+                sum += Tail.Value;
+                Tail = Tail.PreviousNode;
+            }
+            Tail = temp;
+            return sum;
+        }
+
+        public int find(dynamic node)
+        {
+            int indexof = -1;
+            var temp = Head;
+
+            for (int i = 0; ; i++)
+            {
+                if (Head.Value == node)
+                {
+                    indexof = i;
+                    break;
+                }
+
+                else if (Head.Value == null)
+                {
+                    break;
+                }
+
+                Head = Head.NextNode;
+            }
+            Head = temp;
+            return indexof;
+        }
+
+        public void remove(dynamic node, int totalCount)
+        {
+            var temp = Head;
+
+            for (int i = 0; ; i++)
+            {
+                if (Head.Value == node)
+                {
+                    for (int j = i; j < totalCount-1; j++)
+                    {
+                        Head.Value = Head.NextNode.Value;
+
+                        Head = Head.NextNode;
+                    }
+                    break;
+                }
+
+                else
+                {
+                    Head = Head.NextNode;
+                }
+            }
+
+            Head = temp;
+
+        }
+
+
+        public void clear()
+        {
+            Head = Tail = null;
+        }
+
+    }
+}
+
---
+++ CustomLinkedList/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Data_Structures;
+
+var newList = new List<int>();
+
+var mySecondList = new LinkedList<int>();
+
+
+var myList = new CustomLinkedList<int>();
+
+for (int i = 0; i < 20; i++)
+{
+    myList.AddItem(i+1);
+}
+
+Console.WriteLine($"\n\nThe sum of the first 7 items is {myList.AddFirst(7)}");
+
+Console.WriteLine($"\n\nThe sum of the last 5 elements is {myList.AddLast(5)}");
+
+Console.WriteLine($"\n\nThe index of 15 is {myList.find(15)}");
+
+Console.Write($"\n\nThe list after removing 15 is: ");
+myList.remove(15,20);
+var temp = myList.Head;
+
+for (int i = 0; i < 19; i++)
+{
+    Console.Write(myList.Head.Value+", ");
+    myList.Head= myList.Head.NextNode;
+}
+
+myList.Head = temp;
+
+Console.Write($"\n\nThe list after removing 7 is: ");
+myList.remove(7, 19);
+temp = myList.Head;
+
+for (int i = 0; i < 18; i++)
+{
+    Console.Write(myList.Head.Value + ", ");
+    myList.Head = myList.Head.NextNode;
+}
+
+
+myList.Head = temp;
+
+myList.clear();
+
+for(int i = 0; ; i++)
+{
+    if (myList.Head == null)
+    {
+        Console.WriteLine("\n\nThis List is empty ");
+        break;
+    }
+}
+
+
+
+
+
+
