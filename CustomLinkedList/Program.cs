033700f55cae9312e6286297977bf306afee7270
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-05-11 10:42:16 +0100

    Updated Stack&Queue

--- CustomLinkedList/Program.cs
+++ CustomLinkedList/Program.cs
@@ -12,7 +12,7 @@ var myList = new CustomLinkedList<int>();
 
 for (int i = 0; i < 20; i++)
 {
-    myList.AddItem(i+1);
+    myList.AddItem(i + 1);
 }
 
 Console.WriteLine($"\n\nThe sum of the first 7 items is {myList.AddFirst(7)}");
@@ -21,43 +21,69 @@ Console.WriteLine($"\n\nThe sum of the last 5 elements is {myList.AddLast(5)}");
 
 Console.WriteLine($"\n\nThe index of 15 is {myList.find(15)}");
 
-Console.Write($"\n\nThe list after removing 15 is: ");
-myList.remove(15,20);
+Console.Write($"\n\nThe list after removing [15] is: ");
+myList.remove(15, 20);
 var temp = myList.Head;
 
 for (int i = 0; i < 19; i++)
 {
-    Console.Write(myList.Head.Value+", ");
-    myList.Head= myList.Head.NextNode;
+    Console.Write($"[{myList.Head.Value}]  ");
+    myList.Head = myList.Head.NextNode;
 }
 
 myList.Head = temp;
 
-Console.Write($"\n\nThe list after removing 7 is: ");
+Console.Write($"\n\nThe list after removing [7] is: ");
 myList.remove(7, 19);
 temp = myList.Head;
 
 for (int i = 0; i < 18; i++)
 {
-    Console.Write(myList.Head.Value + ", ");
+    Console.Write($"[{myList.Head.Value}]  ");
     myList.Head = myList.Head.NextNode;
 }
 
 
 myList.Head = temp;
 
-myList.clear();
 
-for(int i = 0; ; i++)
+stack<int> myStack = new stack<int>();
+Random rand = new Random();
+
+for (int i = 0; i < 10; i++)
 {
-    if (myList.Head == null)
-    {
-        Console.WriteLine("\n\nThis List is empty ");
-        break;
-    }
+    myStack.push(rand.Next(1, 25));
+}
+myStack.print(10);
+
+dynamic ts = myStack.pop();
+
+myStack.print(9);
+Console.Write($"\tand as you can see, [{ts}] has been removed");
+
+Console.WriteLine($"\n\nThe last element in your stack is currently [{myStack.peek()}]");
+
+
+queue<int> myqueue = new queue<int>();
+for (int i = 0; i < 10; i++)
+{
+    myqueue.enqueue(rand.Next(1, 25));
 }
 
+myqueue.print(10);
 
+myqueue.enqueue(54);
+
+ts=myqueue.dequeue();
+
+myqueue.print(10);
+Console.Write($"\tand as you can see [{ts}] has been removed");
+
+Console.WriteLine($"\n\nIt is {myqueue.contains(12,10)} that my queue contains [12]");
+
+myqueue.clear();
+
+myqueue.print(10);
 
 
 
---
+++ CustomLinkedList/STACK_and_QUEUE.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures
+{
+    public class stack<T> : Options
+    {
+        public ListNode<T> Head { get; set; }
+        public ListNode<T> Tail { get; set; }
+
+        public override void print(int count)
+        {
+            if (Tail is null)
+            {
+                Console.WriteLine("\n\nThis queue is empty");
+                return;
+            }
+            var temp = Tail;
+
+            Console.Write("\n\nMy stack is currently: ");
+            for (int i = 0; i < count; i++)
+            {
+                if (Tail == null) { break; }
+                Console.Write($"[{Tail.Value}]  ");
+                Tail = Tail.NextNode;
+            }
+
+            Tail = temp;
+        }
+
+        public void push(T item)
+        {
+            if (Tail is null)
+            {
+                Tail = Head = new ListNode<T>(item);
+            }
+            else
+            {
+                var temp = Head;
+                var newNode = new ListNode<T>(item, temp);
+                Head.NextNode = newNode;
+                Head = newNode;
+            }
+        }
+
+        public T pop()
+        {
+            if (Tail is null) { return default(T); }
+            else
+            {
+                T popVal = Head.Value;
+                Head = Head.PreviousNode;
+                return popVal;
+            }
+
+        }
+
+        public T peek()
+        {
+            return Head.Value;
+        }
+    }
+
+
+    abstract public class Options
+    {
+        public abstract void print(int count);
+    }
+
+
+    public class queue<T> : Options
+    {
+        public ListNode<T> Head { get; set; }
+        public ListNode<T> Tail { get; set; }
+
+        public override void print(int count)
+        {
+            if(Tail is null)
+            {
+                Console.WriteLine("\n\nThis queue is empty");
+                return;
+            }
+            var temp = Tail;
+
+            Console.Write("\n\nMy queue is currently: ");
+            for (int i = 0; i < count; i++)
+            {
+                if (Tail == null) { break; }
+                Console.Write($"[{Tail.Value}]  ");
+                Tail = Tail.NextNode;
+            }
+
+            Tail = temp;
+        }
+
+        public void enqueue(T item)
+        {
+            if (Tail is null)
+            {
+                Tail = Head = new ListNode<T>(item);
+            }
+            else
+            {
+                var temp = Head;
+                var newNode = new ListNode<T>(item, temp);
+                Head.NextNode = newNode;
+                Head = newNode;
+            }
+        }
+
+        public T dequeue()
+        {
+            if (Tail is null) { return default(T); }
+            else
+            {
+                T popVal = Tail.Value;
+                Tail = Tail.NextNode;
+                return popVal;
+            }
+
+        }
+
+        public bool contains(dynamic item, int count)
+        {
+            bool condition = false;
+            var temp = Tail;
+            for (int i = 0; i < count; i++)
+            {
+                if (Tail.Value == item)
+                {
+                    condition = true;
+                    break;
+                }
+
+                else { Tail = Tail.NextNode; }
+            }
+            Tail = temp;
+            return condition;
+        }
+
+        public void clear()
+        {
+            Head = Tail = null;
+        }
+    }
+}
